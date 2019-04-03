9a86c82f543ba11cccfb95ee9049619721a83a32
Aakash Naik <aakash_a_naik@yahoo.com>
2019-04-02 22:42:10 -0400

    Initial Repo checkin

---
+++ HomeProject.cpp
@@ -0,0 +1,12 @@
+﻿// HomeProject.cpp : Defines the entry point for the application.
+//
+
+#include "HomeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
